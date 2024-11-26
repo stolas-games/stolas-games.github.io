3634b1e06b16a9e378d2a46eaf27e3764a75fe39
kemms-production <kemms_yt@outlook.com>
2024-11-26 20:40:30 +0100

    Create service-worker.js

---
+++ js/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "stolas-cache-v1.58.7";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/main.js",
+  "/assets/images/Stolas-production-logo-transparent.png"
+];
+
+// Installation du Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activation du Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes réseau
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
